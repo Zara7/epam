4c7955af6e77904ba30c63fde615de58dcb83836
Zara Aghababyan <zara.aghababyan@gmail.com>
2024-04-13 16:59:23 +0400

    Devided one big test into several tests

--- test/specs/task2.js
+++ test/specs/task2.js
@@ -1,42 +1,44 @@
-const PastebinPage = require("../pageobjects/PastebinPage.js")
+const PastebinPage = require("../pageobjects/PastebinPage.js");
+
 describe("Create New Paste on Pastebin", () => {
-    before(async () => {
-        await browser.url("https://pastebin.com/");
+    beforeEach(async () => {
+        await PastebinPage.open();
+        await PastebinPage.login("Zara7", "ALASHKERT1!2!3!");
+        await PastebinPage.createNewPast();
     });
 
-   
-    it("Should create a new paste", async () => {
-        await PastebinPage.login("Zara7", "ALASHKERT1!2!3!");
-
-        await PastebinPage.createNewPast();
-
+    it("should set paste text", async () => {
         await PastebinPage.setPasteText(`git config --global user.name  "New Sheriff in Town"
 git reset $(git commit-tree HEAD^{tree} -m "Legacy code")
 git push origin master --force
         `);
-        await PastebinPage.selectSyntax("Bash")
-        await PastebinPage.selectExpiration("10 Minutes");
-        await PastebinPage.setPasteName("how to gain dominance among developers");
-        await PastebinPage.createNewPaste();
-        
-        const actualTitle = await PastebinPage.getInfoTopText();
-
-        const browserTitle = await browser.getTitle();
-        expect(browserTitle).toContain(actualTitle);
-
         const code = await PastebinPage.getCodeText();
-
         const textareaElement = await $('.textarea.-raw.js-paste-raw');
         const textareaText = await textareaElement.getValue();
-
         expect(code).toContain(textareaText);
+    });
 
-        const expire = await PastebinPage.getExpireText();
-        expect(expire).toContain("10 MIN");
-
+    it("should select syntax", async () => {
+        await PastebinPage.selectSyntax("Bash");
         const format = await PastebinPage.getFormatText();
         expect(format).toContain("Bash");
+    });
 
-        await browser.pause(12000);
+    it("should select expiration", async () => {
+        await PastebinPage.selectExpiration("10 Minutes");
+        const expire = await PastebinPage.getExpireText();
+        expect(expire).toContain("10 MIN");
+    });
+
+    it("should set paste name", async () => {
+        await PastebinPage.setPasteName("how to gain dominance among developers");
+
+        const actualTitle = await PastebinPage.getInfoTopText();
+        const browserTitle = await browser.getTitle();
+        expect(browserTitle).toContain(actualTitle);
+    });
+
+    it("should create a new paste", async () => {
+        await PastebinPage.createNewPaste();
     });
 });
